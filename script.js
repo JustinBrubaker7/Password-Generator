f7fb5019a1696540adc21edef7202728b2f920ef
Justin Brubaker <justinbrubaker7@gmail.com>
2021-02-16 12:26:16 -0800

    password generator without promts

--- script.js
+++ script.js
@@ -7,8 +7,32 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//my code start
+
+//Genrate password function
+function generatePassword() {
+  function passwordLength(){
+    length = prompt("How many charters you want?");
+    return length;
+  };
+
+  passwordLength();
+
+  var passcode = "";
+  var values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_";
+
+
+
+  for (var i = 0; i <= length; i++) {
+    var random = Math.floor(Math.random() * values.length);
+    passcode += values.charAt(random);
+
+  }
+return passcode;
+
+}
